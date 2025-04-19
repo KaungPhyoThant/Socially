11d77de9814f2b6db8f1eb8113f51e11e28bf39a
Kaung <104748803+KaungPhyoThant@users.noreply.github.com>
2025-04-19 21:40:27 +0630

    init

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -24,5 +24,109 @@ export async function createPost(content:string , imageUrl:string){
 }
 
 export async function getPosts(){
+    try {
+        const posts = await prisma.post.findMany({
+            orderBy:{
+                createdAt:"desc"
+            },
+            include:{
+                author:{
+                    select:{
+                        name:true,
+                        image:true,
+                        username:true,
+                    }
+                },
+                comments:{
+                    include:{
+                        author:{
+                            select:{
+                                id:true,
+                                username:true,
+                                image:true,
+                                name:true
+                            }
+                        }
+                    },
+                    orderBy:{
+                        createdAt:"desc"
+                    }
+                },
+                likes:{
+                    select:{
+                        userId:true
+                    }
+                },
+                _count:{
+                    select:{
+                        likes:true,
+                        comments:true
+                    }
+                }
+            }
+        })
+        return posts;
+    } catch (error) {
+        console.log("Error in getPosts:",error);
+        throw new Error("Failed to fetch posts.")
+    }
+}
 
+export async function toggleLike(postId:string) {
+    try {
+        const userId = await getDbUserId();
+        if(!userId) return ;
+
+        const existingLike = await prisma.like.findUnique({
+            where:{
+                userId_postId:{
+                    userId,
+                    postId,
+                },
+            }
+        });
+
+        const post = await prisma.post.findUnique({
+            where:{id:postId},
+            select:{authorId:true},
+        });
+
+        if(!post) throw new Error("Post not found.");
+
+        if(existingLike){
+            await prisma.like.delete({
+                where:{
+                    userId_postId:{
+                        userId,
+                        postId,
+                }
+            }
+        })
+    }else{
+        await prisma.$transaction([
+            prisma.like.create({
+                data:{
+                    userId,
+                    postId,
+                }
+            }),
+            ...(post.authorId !== userId
+            ? [
+                prisma.notification.create({
+                    data:{
+                        type:"LIKE",
+                        userId:post.authorId,
+                        creatorId:userId,
+                        postId,
+                    }
+                })
+            ] : []),
+        ])
+    }
+    revalidatePath("/");
+    return {success:true};
+    } catch (error) {
+        console.log("Failed to toggle like:",error);
+        return {success:false,error:"Failed to toggle like."}
+    }
 }
